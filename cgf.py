b52035066857427a82667f01f3c02dd8ebc11700
randomuser149 <asdimasdi15@gmail.com>
2025-10-21 22:37:47 +0200

    Dictionary is fixed, funtion correctly reads parameterized input file into the dictionary. Added comments for clarity.

--- cgf.py
+++ cgf.py
@@ -1,11 +1,15 @@
-with open("input.txt","r") as file:
-    grammar = {}
-    nonterminals = set()
-    for line in file:
-        left_hand_side, right_hand_side = line.strip().split("->",1)
-        left_hand_side = left_hand_side.strip()
-        right_hand_side = [item.strip() for item in right_hand_side.split("|")]
-        nonterminals.add(line[0].strip())
-        for rule in right_hand_side:
-            grammar[line[0].strip()]=rule.strip() # overwrites prev definitons, need fix to update if key is present
-            
+def parse_rules(filename):
+    with open(filename) as file:
+        grammar = {}
+        nonterminals = set()
+        for line in file:
+            if "->" not in line:
+                continue
+            left_hand_side, right_hand_side = line.strip().split("->",1)
+            left_hand_side = left_hand_side.strip()
+            nonterminals.add(left_hand_side)
+            for rule in [item.strip() for item in right_hand_side.split("|")]:
+                grammar.setdefault(left_hand_side, []).append(rule)
+    return dict(grammar), nonterminals
+
+parse_rules("input.txt")
